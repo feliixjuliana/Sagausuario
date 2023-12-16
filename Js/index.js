6f2f9c3aae8125f83d6dbeb1ceb7b9ec3a48fdda
Cor <felixjuliana890@gmail.com>
2023-12-16 09:43:49 -0300

    nova versao

--- Js/index.js
+++ Js/index.js
@@ -2,5 +2,5 @@
 function codigoNum(){
     var nota = parseInt(prompt("Informe o código enviado para seu número"));
 
-    alert ("Agendamento concluído");
+    alert("Agendamento concluído para o dia \n" + data.value + "\n Paciente: "+ paciente.value + "\n Responsável: "+ nome.value + "\n Número: "+ nome.value);
 }
