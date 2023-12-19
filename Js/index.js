5b9d348a2f62eed349d120a9ac9b4d99ed2004b9
Cor <felixjuliana890@gmail.com>
2023-12-18 21:50:44 -0300

    Pagina da medica

--- Js/index.js
+++ Js/index.js
@@ -1,6 +1,8 @@
 
 function codigoNum(){
-    var nota = parseInt(prompt("Informe o código enviado para seu número"));
+    var nota = parseInt(prompt("Informe o código enviado para seu número:"));
 
-    alert("Agendamento concluído para o dia \n" + data.value + "\n Paciente: "+ paciente.value + "\n Responsável: "+ nome.value + "\n Número: "+ nome.value);
+    alert("Agendamento concluído!");
+
+
 }
